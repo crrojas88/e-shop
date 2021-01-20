6e479c6e24911933ed31368ffacc068fad9b39df
crrojas88 <carlorrojas@gmail.com>
2021-01-19 16:01:03 -0800

    added Cart context

--- context/Cart.js
+++ context/Cart.js
@@ -3,9 +3,45 @@ import { createContext } from 'react'
 export const Context = createContext()
 
 const Cart = ({ children }) => {
-    const exposed = {
-        test: 'Dooks',
+    const getInitialCart = () => JSON.parse(localStorage.getItem('cart'))
+    const [cart, setCart] = useState([])
+
+    useEffect(() => {
+        const initialCart = getInitialCart()
+        if (initialCart) {
+            setCart(initialCart)
+        }
+    }, [])
+
+    useEffect(() => {
+        // write to local storage
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
+    const addItemToCart = (id, qty = 1) => {
+        const item = cart.find(i => i.id === id)
+        if (item) {
+            // increase qty
+            item.qty += qty
+            setCart([...cart])
+        } else {
+            setCart([...cart, { id, qty }])
+        }
     }
+
+    const removeItemFromCart = (id) => {
+        const newCart = cart.filter(item => {
+            return item.id !== id
+        })
+        setCart(newCart)
+    }
+
+    const exposed = {
+        cart,
+        addItemToCart,
+        removeItemFromCart,
+    }
+    
     return <Context.Provider value={exposed}>{children}</Context.Provider>
 }
 
